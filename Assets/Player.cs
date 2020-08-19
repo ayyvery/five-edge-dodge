919bf7a92a0f51b930e5f428053c2c0486d59be8
wutperson <yudin.vladd@gmail.com>
2020-08-19 21:12:44 +0300

    First commit

---
+++ Assets/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int speed = 500;
+
+    float movement = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, movement * speed * Time.deltaTime * -1);
+    }
+}
